5a901ad6ff294a7efe4402934059082deba5a909
AdnanOlarmmi <olarmmi@gmail.com>
2022-12-06 20:06:36 +0100

    Find Neutered Animals

--- queries.sql
+++ queries.sql
@@ -9,3 +9,5 @@ SELECT * FROM animals WHERE neutered AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon','Pikachu');
 
 SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
+
+SELECT * FROM animals WHERE neutered;
