7369417eba18c938a6e65eea081348b83e0ef55b
AdnanOlarmmi <olarmmi@gmail.com>
2022-12-06 20:04:01 +0100

    Find neutered and Escaped animals

--- queries.sql
+++ queries.sql
@@ -2,4 +2,6 @@
 
 SELECT * FROM animals WHERE name LIKE '%mon';
 
- SELECT * from animals WHERE date_of_birth BETWEEN '2016/01/01' AND '2019/12/12';
+SELECT * from animals WHERE date_of_birth BETWEEN '2016/01/01' AND '2019/12/12';
+
+SELECT * FROM animals WHERE neutered AND escape_attempts < 3;
