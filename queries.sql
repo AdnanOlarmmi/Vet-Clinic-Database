350b7d20be49d32af0004d06a844730b1dd9dda9
AdnanOlarmmi <olarmmi@gmail.com>
2022-12-06 20:06:03 +0100

    Weight and escape attempts of big animals

--- queries.sql
+++ queries.sql
@@ -7,3 +7,5 @@ SELECT * from animals WHERE date_of_birth BETWEEN '2016/01/01' AND '2019/12/12';
 SELECT * FROM animals WHERE neutered AND escape_attempts < 3;
 
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon','Pikachu');
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
