f50705103c9e345309a4454ec2a1f85a7f719e69
AdnanOlarmmi <olarmmi@gmail.com>
2022-12-06 20:07:18 +0100

    Animals not Named Gabumon

--- queries.sql
+++ queries.sql
@@ -11,3 +11,5 @@ SELECT date_of_birth FROM animals WHERE name IN ('Agumon','Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
 
 SELECT * FROM animals WHERE neutered;
+
+SELECT * FROM animals WHERE NOT name='Gabumon';
