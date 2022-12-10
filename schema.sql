6d6db99c611f571cb307e75a11054701ae6dc1b3
AdnanOlarmmi <olarmmi@gmail.com>
2022-12-10 11:08:14 +0100

    Create  Table Schema

--- schema.sql
+++ schema.sql
@@ -14,3 +14,26 @@ ALTER TABLE animals DROP species;
 ALTER TABLE animals ADD species_id INT REFERENCES species;
 
 ALTER TABLE animals ADD owner_id INT REFERENCES owners;
+
+
+--Create a table named vets:
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(150) NOT NULL,
+    age INT NOT NULL,
+    date_of_graduation DATE NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations (
+    species_id INTEGER REFERENCES species(id),
+    vet_id INTEGER REFERENCES vets(id),
+    PRIMARY KEY (species_id, vet_id)
+);
+
+CREATE TABLE visits (
+    animal_id INTEGER REFERENCES animals(id),
+    vet_id INTEGER REFERENCES vets(id),
+    visit_date date,
+    PRIMARY KEY (animal_id, vet_id)
+);
